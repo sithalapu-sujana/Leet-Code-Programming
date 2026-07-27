8891f637687626b3e7f31d9886d7b2792dd314f2
sithalapu-sujana <sujanasumanasujana@gmail.com>
2026-07-27 11:34:54 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -3,4 +3,16 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
+                    return [i, j]     #o(n**2)
+
+class Solution:
+    def twoSum(self, nums, target):
+        d = {}
+
+        for i in range(len(nums)):
+            need = target - nums[i]
+
+            if need in d:
+                return [d[need], i]
+
+            d[nums[i]] = i #o(n) best
